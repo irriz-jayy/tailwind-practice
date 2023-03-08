56df17fcd5dbc0386b7b53780a27f714c65b6288
irriz-jayy <jayfelix789@gmail.com>
2023-03-08 17:56:27 +0300

    rendered login

--- src/components/Login.js
+++ src/components/Login.js
@@ -8,21 +8,12 @@ export default function Login() {
           <div>
             <img
               className="mx-auto h-12 w-auto"
-              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+              src="https://raw.githubusercontent.com/irriz-jayy/images/main/logo.png"
               alt="Your Company"
             />
             <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
               Sign in to your account
             </h2>
-            <p className="mt-2 text-center text-sm text-gray-600">
-              Or{" "}
-              <a
-                href="#"
-                className="font-medium text-indigo-600 hover:text-indigo-500"
-              >
-                start your 14-day free trial
-              </a>
-            </p>
           </div>
           <form className="mt-8 space-y-6" action="#" method="POST">
             <input type="hidden" name="remember" defaultValue="true" />

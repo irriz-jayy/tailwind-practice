df7a8ddbccbcc8cde0ee58fb702ef275cf8f26a0
irriz-jayy <jayfelix789@gmail.com>
2023-03-08 18:02:48 +0300

    Added routes and routed login form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Login from "./components/Login";
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </div>
   );
 }
